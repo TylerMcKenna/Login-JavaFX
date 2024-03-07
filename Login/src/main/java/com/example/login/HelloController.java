86d9a75909b0f315c119ccb866893e04f1a61a8c
TylerMcKenna <tylermckenna01@gmail.com>
2024-03-07 00:16:08 -0600

    Completed a basic login page using FXML and CSS

--- Login/src/main/java/com/example/login/HelloController.java
+++ Login/src/main/java/com/example/login/HelloController.java
@@ -2,10 +2,19 @@ package com.example.login;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private void initialize() {
+        anchorPane.getStyleClass().add("pane");
+    }
 
     @FXML
     protected void onHelloButtonClick() {
