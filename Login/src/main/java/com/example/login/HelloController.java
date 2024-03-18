5704908b4bc3781b123c25d95bd61cc806db741b
TylerMcKenna <tylermckenna01@gmail.com>
2024-03-18 18:53:52 -0500

    got login working no encryption though:

--- Login/src/main/java/com/example/login/HelloController.java
+++ Login/src/main/java/com/example/login/HelloController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -20,16 +21,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class HelloController {
     private static ArrayList<User> userList = new ArrayList<User>();
 
     @FXML
-    private PasswordField passFieldSU;
+    private PasswordField passFieldSU, passFieldSI;
 
     @FXML
-    private TextField txtUsernameSU, txtEmailSU;
+    private TextField txtUsernameSU, txtEmailSU, txtUsernameSI;
 
     @FXML
     private AnchorPane anchorPane;
@@ -40,7 +43,7 @@ public class HelloController {
     // https://stackoverflow.com/questions/32268733/using-jackson-to-manually-parse-json
     @FXML
     private void initialize() throws IOException {
-        // Sets id to last id in the json file + 1
+        /*// Sets id to last id in the json file + 1
         try {
             JsonParser jsonParser = new JsonFactory().createParser(new File("users.json"));
 
@@ -65,7 +68,7 @@ public class HelloController {
         File file = new File("users.json");
 
         List<User> employeeList = objectMapper.readValue(file, new TypeReference<>(){});
-
+*/
         anchorPane.getStyleClass().add("pane");
         groupSignUp.setVisible(false);
 
@@ -83,8 +86,34 @@ public class HelloController {
         groupSignUp.setVisible(true);
     }
 
+    public void signInPressed(ActionEvent actionEvent) {
+        User[] users = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String json = "users.json".toString();
+            users = objectMapper.readValue(new File("users.json"), User[].class);
+            System.out.println(Arrays.toString(users));
+        } catch (JsonProcessingException e) {
+            System.out.println("I'm cooked");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("I'm cooked #2");
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < users.length; i++) {
+            User temp = new User(txtUsernameSI.getText(), passFieldSI.getText(), null, users[i].getSalt());
+            if (Objects.equals(temp.getEncryptedUsername(),users[i].getEncryptedUsername())) {
+                if (Objects.equals(temp.getHashedPassword(), users[i].getHashedPassword())) {
+                    System.out.println("LETS GO!");
+                }
+            }
+        }
+    }
+
     public void signUpPressed(ActionEvent actionEvent) throws IOException {
-        userList.add(new User(txtUsernameSU.getText(), txtEmailSU.getText(), passFieldSU.getText()));
+        userList.add(new User(txtUsernameSU.getText(), passFieldSU.getText(), txtEmailSU.getText()));
+
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
--- Login/src/main/java/com/example/login/User.java
+++ Login/src/main/java/com/example/login/User.java
@@ -5,143 +5,71 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import org.mindrot.jbcrypt.BCrypt;
 
-import java.io.FileReader;
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
-import java.util.Arrays;
-import java.util.Base64;
-
 public class User {
-    private static int currentID = 0;
+    public String encryptedUsername, hashedPassword, encryptedEmail, salt;
+/*[ {
+  "encryptedUsername" : null,
+  "hashedPassword" : "$2a$10$zCXQSGiJbH/I0fp36cJA6u5eZ8FH9NNddZ7uVx/DvWYD9q2wPjeDW",
+  "encryptedEmail" : null,
+  "salt" : "$2a$10$zCXQSGiJbH/I0fp36cJA6u"
+}
+  {
+  "encryptedUsername" : null,
+  "hashedPassword" : "$2a$10$dME2/DzRTte8WvKBcp7ZcuJzttxbaIaq8XcXuu95kOSmlz.yx2sPW",
+  "encryptedEmail" : null,
+  "salt" : "$2a$10$dME2/DzRTte8WvKBcp7Zcu"
+} ]*/
+    public User() {
 
-    private int id;
-
-    private String name, nameKey, email, emailKey, password, passwordSalt;
-
-    public User(String name, String email, String password) {
-        try {
-            this.id = currentID;
-            currentID++;
-
-            KeyValue nameKeyVal = getEncryption(name);
-            this.name = nameKeyVal.getValue().toString();
-            this.nameKey = Base64.getEncoder().encodeToString(nameKeyVal.getKey().getEncoded());
-
-            KeyValue emailKeyVal = getEncryption(name);
-            this.email = emailKeyVal.getValue().toString();
-            this.emailKey = Base64.getEncoder().encodeToString(emailKeyVal.getKey().getEncoded());
-
-            SaltValue passwordSaltVal = getHash(password);
-            this.password = passwordSaltVal.getValue();
-            this.passwordSalt = passwordSaltVal.getSalt();
-        } catch (Exception e) {
-            System.out.println("I'm cooked");
-            e.printStackTrace();
-            System.exit(0);
-        }
     }
 
-    private KeyValue getEncryption(String text) {
-        try {
-            // Generates a SecretKey using AES
-            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-            keyGen.init(256);
-            SecretKey secretKey = keyGen.generateKey();
-
-            // Create cipher, set its mode, and pass it a key.
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-
-            // Converts text to a byte array and encrypts
-            byte[] plaintext = text.getBytes();
-            byte[] ciphertext = cipher.doFinal(plaintext);
-
-            return new KeyValue(secretKey, ciphertext);
-        } catch (Exception e) {
-            System.out.println("I'm cooked");
-            System.exit(0);
-            return null;
-        }
+    public User(String username, String password, String email) {
+        this.encryptedUsername = username;
+        this.salt = BCrypt.gensalt(10);
+        this.hashedPassword = hash(password);
+        this.encryptedEmail = email;
     }
 
-    /*
-if (BCrypt.checkpw(candidate, hashed))
-    System.out.println("It matches");
-else
-    System.out.println("It does not match");*/
-    private SaltValue getHash(String password) {
-        String salt = BCrypt.gensalt();
-        return new SaltValue(salt, BCrypt.hashpw(password, salt));
+    public User(String username, String password, String email, String salt) {
+        this.encryptedUsername = username;
+        this.salt = salt;
+        this.hashedPassword = hash(password);
+        this.encryptedEmail = email;
     }
 
-    private String getDecryption(KeyValue encrypted) {
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, encrypted.getKey());
-
-            byte[] decryptedText = cipher.doFinal(encrypted.getValue());
-            String text = new String(decryptedText, StandardCharsets.UTF_8);
-            return text;
-        } catch (Exception e) {
-            System.out.println("I'm cooked");
-            System.exit(0);
-            return null;
-        }
+    private String hash(String password) {
+        return BCrypt.hashpw(password, salt);
     }
 
-    public String getName() {
-        return name;
+    // How to use idk yet
+    public boolean checkPassword(String password) {
+        //return BCrypt.hashpw()
+        return true;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public String toString() {
+        return "User{" +
+                "encryptedUsername='" + encryptedUsername + '\'' +
+                ", hashedPassword='" + hashedPassword + '\'' +
+                ", encryptedEmail='" + encryptedEmail + '\'' +
+                ", salt='" + salt + '\'' +
+                '}';
     }
 
-    public String getNameKey() {
-        return nameKey;
+    public String getEncryptedUsername() {
+        return encryptedUsername;
     }
 
-    public void setNameKey(String nameKey) {
-        this.nameKey = nameKey;
+    public String getHashedPassword() {
+        return hashedPassword;
     }
 
-    public String getEmail() {
-        return email;
+    public String getEncryptedEmail() {
+        return encryptedEmail;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEmailKey() {
-        return emailKey;
-    }
-
-    public void setEmailKey(String emailKey) {
-        this.emailKey = emailKey;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPasswordSalt() {
-        return passwordSalt;
-    }
-
-    public void setPasswordSalt(String passwordSalt) {
-        this.passwordSalt = passwordSalt;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public static void setCurrentID(int currentID) {
-        User.currentID = currentID;
+    public String getSalt() {
+        return salt;
     }
 }
